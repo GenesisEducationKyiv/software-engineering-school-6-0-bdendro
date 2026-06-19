007ad7614e264eb61603cf3fe9e1a7c309f86df9
bdendro <olegburia2072@gmail.com>
2026-06-19 20:46:21 +0300

    fix: update notification contact imports

--- src/infrastructure/notification/notification.client.ts
+++ src/infrastructure/notification/notification.client.ts
@@ -11,7 +11,7 @@ import {
   SendUnsubscribeSuccessBody,
   ServiceUnavailableResponse,
   ValidationErrorResponse,
-} from '../../../libs/contracts/notification/notification.schema';
+} from '../../../libs/contracts/notification/notification.contract';
 import { NOTIFICATION_ROUTE_PATHS } from '../../../libs/contracts/notification/notification.const';
 import { EmailServiceError } from '../../../libs/common/utils/errors/custom-errors';
 
