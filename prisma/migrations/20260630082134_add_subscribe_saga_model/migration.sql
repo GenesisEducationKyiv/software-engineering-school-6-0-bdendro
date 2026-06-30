fb4caf60e0dba76b3612ef2882281bc8caf90647
bdendro <olegburia2072@gmail.com>
2026-06-30 11:23:44 +0300

    feat: add subscribe saga model

---
+++ prisma/migrations/20260630082134_add_subscribe_saga_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "subscribe_sagas" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "repo_name" TEXT NOT NULL,
+    "subscription_id" INTEGER,
+    "repo_id" INTEGER,
+    "state" TEXT NOT NULL DEFAULT 'STARTED',
+    "error_message" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "subscribe_sagas_pkey" PRIMARY KEY ("id")
+);
